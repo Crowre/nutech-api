fc05e6faa379ba44b6f617fc1091aa44b1885a5c
Wildan Auliya Hafidz <110149128+Crowre@users.noreply.github.com>
2025-10-27 09:29:41 +0700

    finalisasi kodingan controller untuk profile user
    terdapat keterbatasan server vercel dalam mengupload file foto

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -40,7 +40,7 @@ const handleFileUpload = (file) => {
             if (!allowedFormats.includes(file.mimetype) ||
                 !allowedExtensions.includes(fileExtension) ||
                 file.size > 2 * 1024 * 1024) {
-                // fs.unlinkSync(file.path);     //sementara ini dinonaktifkan karena masih menggunakan server vercel
+                fs.unlinkSync(file.path);     //program upload foto tidak bisa dieksekusi karena masih menggunakan server vercel yang memiliki keterbatasan untuk menyimpan file secara permanen
                 reject(new Error('FORMAT_ERROR'));
                 return;
             }
